e22dccefe080763799986da957e7409d173d8b24
Wiley Kestner <wiley@aya.yale.edu>
2013-03-22 22:54:40 -0700

    Initial Commit

---
+++ CarlsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarlsApp
+//
+//  Created by wiley on 3/22/13.
+//  Copyright (c) 2013 Prairiedogg International Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarlsApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CarlsApp
+//
+//  Created by wiley on 3/22/13.
+//  Copyright (c) 2013 Prairiedogg International Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
